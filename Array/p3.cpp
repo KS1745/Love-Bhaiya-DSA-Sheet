5fe5de40a5984e46104f2467c792b24a8c1c6408
KS1745 <kartikay.sinha17@gmail.com>
2024-03-14 23:04:54 +0530

    first commit

--- Array/p3.cpp
+++ Array/p3.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-  
+int kthSmallest(int arr[], int l, int r, int k) {
+        
+        priority_queue<int>pq;
+        for(int i=l;i<=r;i++){
+            pq.push(arr[i]);
+            if(pq.size() > k) 
+                pq.pop();
+        }
+        return pq.top();
+        
 }
